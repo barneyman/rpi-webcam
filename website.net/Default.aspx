a289c15c39983d912d12436b4bfb39259efd3715
barneyman <barnaby.james.flint@gmail.com>
2015-06-29 18:28:34 +1000

    added website

---
+++ website.net/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <META HTTP-EQUIV="refresh" CONTENT="60"/>
+</head>
+<body onload="">
+    Latest Picture
+
+    <form id="form1" runat="server">
+    <div>
+    <img src="~\latest.jpg"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ website.net/upload.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class upload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        // this is called by a POST request ... so check that
+            if (Request.HttpMethod == "POST")
+            {
+                System.Data.SqlClient.SqlConnection m_sqlConnection;
+
+                string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["picConnect"].ConnectionString;
+
+                m_sqlConnection = new System.Data.SqlClient.SqlConnection(connStr);
+
+                m_sqlConnection.Open();
+
+                // prepare the statement
+                System.Data.SqlClient.SqlCommand insertCmd = new System.Data.SqlClient.SqlCommand("addPicture", m_sqlConnection);
+                insertCmd.CommandType = CommandType.StoredProcedure;
+
+                string hostname = "_unknown_";
+                if (Request.Params["hostname"] != null)
+                    hostname = Request.Params["hostname"];
+
+                insertCmd.Parameters.Add("@hostname", hostname);
+
+                byte[] buffer = new byte[Request.InputStream.Length];
+                Request.InputStream.Read(buffer, 0, buffer.Length);
+
+                string b64 = System.Text.Encoding.Default.GetString(buffer);
+
+                byte[] image = Convert.FromBase64String(b64);
+                insertCmd.Parameters.Add("@pic", image);
+
+                insertCmd.ExecuteNonQuery();
+
+                m_sqlConnection.Close();
+            }
+ 
+            
+    }
+}
